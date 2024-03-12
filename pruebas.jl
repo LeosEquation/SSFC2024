c58cec696be7fd34cfcc97d70511567ff09fb26b
Leonel Mayorga Lopez <leoml141.lml@ciencias.unam.mx>
2024-03-12 10:45:06 -0600

    Respaldo de una variable

--- pruebas.jl
+++ pruebas.jl
@@ -1,7 +1,11 @@
 include("psarc.jl")
 using .PArcLength, Plots
 
+#-
+
 g(u,λ) = (u^2-1)*(u^2-4) + λ*u^2*exp(u/10)
+
+#-
 
 x_ini = [2.0,1.0,-1.0,-2.0]
 p_ini = 0.0
@@ -9,6 +13,10 @@ p_ini = 0.0
 p_fin = 1.0
 orden = 10
 
+#-
+
 solfams = SolFam.(g, x_ini, p_ini, Δs, p_fin,orden)
+
+#-
 
 plot(solfams, leg = false, title = "Familias de Soluciones", ylabel = "u(λ)", xlabel = "λ", c = "blue")
