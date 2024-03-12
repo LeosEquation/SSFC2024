8e1765654d42894350aac1040b68c907e96e77b8
Leonel Mayorga López <leoml141.lml@gmail.com>
2024-03-11 21:21:40 -0600

    Actualización rama familysolutions

---
+++ pruebas.jl
@@ -0,0 +1,12 @@
+include("psarc.jl")
+using .PArcLength, Plots
+
+g(u,λ) = (u^2-1)*(u^2-4) + λ*u^2*exp(u/10)
+
+x_ini = [2.0,1.0,-1.0,-2.0]
+p_ini = 0.0
+Δs= 0.001
+p_fin = 1.0
+orden = 10
+
+solfams = SolFam.(g, x_ini, p_ini, Δs, p_fin,orden)
