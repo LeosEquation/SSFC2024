b16c332ce301e5d2605fba409cc34758618008f2
Leonel Mayorga López <leoml141.lml@gmail.com>
2024-05-13 21:54:07 -0600

    Añadi el metodo para puntos límite (Limit Points) y añadi el sistema y su jacobiano correspondiente a las bifurcaciones de Hopf a falta de un metodo que encuentre el punto

--- src/bifurcation.jl
+++ src/bifurcation.jl
@@ -38,36 +38,6 @@ end
 #-
 
 """
-    Bifurcation_point(f::Function,x0::Float64,p0::Float64,t::Float64; type_bif = false)
-
-Esta función devuelve el punto de bifurcación `p::Float64, x::Float64` más cercano a `x0` y `p0`
-de la ecuación diferencial descrita por `f`. Si `type_bif = true` se imprimirá el tipo de bifurcación.
-"""
-function Bifurcation_point(f!::Function,x0::Vector{Float64},p0::Float64,t::Float64; type_bif = false)
-    S = set_variables("s",numvars = length(x0)+1,order = 3)
-    x = x0 + S[1:end-1]
-    p = p0 + S[end]
-    ceros = zeros(length(x0)+1)
-    dx = x
-    f!(dx,x,p,t)
-
-    J = [derivative(dx[i],j) for i in 1:length(x), j in 1:length(x)]
-
-    Bif_vec(x,p,t) = [f(x,p,t),]
-    i = 1
-    while i <= 30 && norm(Bif_vec(x,p,t)(ceros)) > 1.e-16
-        x, p = [x,p] - inv(TaylorSeries.jacobian(Bif_vec(x,p,t),ceros))*Bif_vec(x,p,t)(ceros)
-        i += 1
-    end
-    if type_bif
-        Type_Bifurcation(f,x,p,t,ceros)
-    end
-    return p(ceros), x(ceros)
-end
-
-#-
-
-"""
     Bifurcation_point(f::Function,x0::Float64,p0::Float64,t::Float64,indice::Int64; type_bif = false)
 
 Esta función devuelve el punto de bifurcación `p[indice]::Float64, x::Float64` más cercano a `x0` y `p0[indice]`
@@ -202,6 +172,285 @@ function Type_Bifurcation(f::Function,x::TaylorN,p::TaylorN,t::Float64,ceros::Ve
 end
 
 #-
+
+function LP_initial_values(f!::Function,equilibrium_branch::Tuple{Vector{Float64}, Vector{Vector{Float64}}},t::Float64)
+
+    n = length(equilibrium_branch[2][1])
+    N_points = length(equilibrium_branch[1])
+    s = Taylor1(1)
+    J_old = zeros(n,n)
+    Indices = Int64[]
+    Eigenvecs = Vector[]
+    Eigenvals = []
+
+    for i in 1:n
+        for j in 1:n
+            dx = [s for i in 1:n]
+            f!(dx,equilibrium_branch[2][1]+[j == k ? s : 0.0 for k in 1:n],equilibrium_branch[1][1],t)
+            J_old[i,j] = derivative(dx[j])(0.0)
+        end
+    end
+    
+    for i in 2:(N_points)
+        J_new = zeros(n,n)
+        for j in 1:n
+            for k in 1:n
+                dx = [s for i in 1:n]
+                f!(dx,equilibrium_branch[2][i]+[k == l ? s : 0.0 for l in 1:n],equilibrium_branch[1][i],t)
+                J_new[j,k] = derivative(dx[j])(0.0)
+            end
+        end
+
+        λ_new = eigvals(J_new)
+        λ_old = eigvals(J_old)
+
+        if !all((real.(λ_new) .* real.(λ_old)) .> 0.0) && !all((imag.(λ_new) .* imag.(λ_old)) .> 0.0)
+            norm_new = findmin(norm.(λ_new))
+            norm_old = findmin(norm.(λ_old))
+            if norm_old[1] < norm_new[1]
+                push!(Indices,i-1)
+                push!(Eigenvals,norm_old[1])
+                push!(Eigenvecs,eigvecs(J_old)[:,norm_old[2]])
+            else
+                push!(Indices,i)
+                push!(Eigenvals,norm_new[1])
+                push!(Eigenvecs,eigvecs(J_new)[:,norm_new[2]])
+            end
+        end
+
+        J_old = J_new
+
+    end
+
+    return [[equilibrium_branch[1][Indices[i]],equilibrium_branch[2][Indices[i]],Eigenvecs[i],Eigenvals[i]] for i in 1:length(Indices)]
+end
+
+#-
+
+function LP_Jacobian(f!::Function,x0::Vector{Float64},p0::Float64,ν0::Union{Vector{Float64},Vector{ComplexF64}},t::Float64; ω = [1.0,1.0])
+
+    n = length(x0)
+    ceros = zeros(n+1)
+
+    S = set_variables("s", numvars = n+1, order = 3)
+
+    x = x0 + S[1:end-1]
+    p = p0 + S[end]
+
+    dx = x
+
+    f!(dx,x,p,t)
+    
+    A = [derivative(dx[i],j)(ceros) for i in 1:n, j in 1:n]
+    B = [derivative(dx[i],n+1)(ceros) for i in 1:n]
+    C = [0.0 for i in 1:n, j in 1:n]
+
+    D = [sum([derivative(derivative(dx[i],k)j)(ceros)*ν0[k] for k in 1:n]) for i in 1:n, j in 1:n]
+    E = [sum([derivative(derivative(dx[i],k)n+1)(ceros)*ν0[k] for k in 1:n]) for i in 1:n]
+    F = A
+    
+    G = zeros(1,n)
+    K = 0.0
+    M = transpose(ω)
+    
+    Extended_Jacobian = [A B C;
+                         D E F;
+                         G K M]
+    
+    return Extended_Jacobian
+end
+
+#-
+
+function LP_Function(f!::Function,x0::Vector{Float64},p0::Float64,ν0::Union{Vector{Float64},Vector{ComplexF64}},t::Float64; ω = [1.0,1.0])
+
+    n = length(x0)
+    ceros = zeros(n+1)
+
+    S = set_variables("s", numvars = n+1, order = 3)
+
+    x = x0 + S[1:end-1]
+    p = p0 + S[end]
+
+    dx = x
+
+    f!(dx,x,p,t)
+
+    LP1 = dx(ceros)
+    LP2 = [sum([derivative(dx[i],k)(ceros)*ν0[k] for k in 1:n]) for i in 1:n]
+    LP3 = ω ⋅ ν0 - 1.0
+
+    return [LP1; LP2; LP3]
+end
+
+#-
+
+function Limit_Points(f!::Function,equilibrium_branch::Tuple{Vector{Float64}, Vector{Vector{Float64}}},Δs::Float64,t::Float64; ω = [1.0,1.0], test = false)
+    
+    initial_values = LP_initial_values(f!,equilibrium_branch,t)
+
+    LPs = Vector{Union{Float64,Vector{Float64}}}[]
+
+    for initial_value in initial_values
+        p0, x0, ν0, λ0 = initial_value
+
+        x = x0
+        p = p0
+        ν = ν0
+
+        i = 1
+
+        while i <= 30 && norm(LP_Function(f!,x,p,ν,t; ω = ω)) > 1.e-16
+
+            if test
+                println("Paso $(i): λ = $(p)")
+                println("           x = $(x)")
+                println("           norm(LP_Function) = $(norm(LP_Function(f!,x,p,ν,t)))")
+                println("\n")
+                show(stdout, "text/plain",  LP_Function(f!,x,p,ν,t))
+                println("\n")
+            end
+
+            X = [x;p;ν] - inv(LP_Jacobian(f!,x,p,ν,t; ω = ω))*LP_Function(f!,x,p,ν,t; ω = ω)
+            x = X[1:length(x)]
+            p = X[length(x)+1]
+            ν = X[length(x)+2:end]
+            i += 1
+        end
+        
+        if norm(LP_Function(f!,x,p,ν,t; ω = ω)) < Δs
+            push!(LPs,[p,x])
+        end
+
+    end
+
+        return LPs
+end
+
+#-
+
+function Hopf_initial_values(f!::Function,equilibrium_branch::Tuple{Vector{Float64}, Vector{Vector{Float64}}},t::Float64)
+
+    n = length(equilibrium_branch[2][1])
+    N_points = length(equilibrium_branch[1])
+    s = Taylor1(1)
+    J_old = zeros(n,n)
+    Indices = Int64[]
+    Eigenvecs = []
+    Eigenvals = []
+    Nullspaces = []
+
+    for i in 1:n
+        for j in 1:n
+            dx = [s for i in 1:n]
+            f!(dx,equilibrium_branch[2][1]+[j == k ? s : 0.0 for k in 1:n],equilibrium_branch[1][1],t)
+            J_old[i,j] = derivative(dx[j])(0.0)
+        end
+    end
+    
+    for i in 2:(N_points)
+        J_new = zeros(n,n)
+        for j in 1:n
+            for k in 1:n
+                dx = [s for i in 1:n]
+                f!(dx,equilibrium_branch[2][i]+[k == l ? s : 0.0 for l in 1:n],equilibrium_branch[1][i],t)
+                J_new[j,k] = derivative(dx[j])(0.0)
+            end
+        end
+
+        λ_new = eigvals(J_new)
+        λ_old = eigvals(J_old)
+
+        if !all((real.(λ_new) .* real.(λ_old)) .> 0.0) && !all((imag.(λ_new) .* imag.(λ_old)) .<= 0.0)
+            if all((imag.(λ_old) .^ 2) .< (imag.(λ_new) .^ 2))
+                push!(Indices,i-1)
+                push!(Eigenvecs,eigvecs(J_old))
+                push!(Nullspaces,eigvecs(transpose(J_old^2 + imag(λ_old[1])^2*I(n))))
+                push!(Eigenvals,λ_old)
+            else
+                push!(Indices,i)
+                push!(Eigenvecs,eigvecs(J_new))
+                push!(Nullspaces,eigvecs(transpose(J_new^2 + imag(λ_new[1])^2*I(n))))
+                push!(Eigenvals,λ_new)
+            end
+        end
+
+        J_old = J_new
+
+    end
+
+    return [[equilibrium_branch[1][Indices[i]],equilibrium_branch[2][Indices[i]],Eigenvecs[i],Eigenvals[i], Nullspaces[i]] for i in 1:length(Indices)]
+end
+
+#-
+
+function Hopf_function(f!::Function,x0,p0,ν0,κ0,t::Float64,w0)
+
+    n = length(x0)
+    ceros = zeros(n+1)
+
+    S = set_variables("s", numvars = n+1, order = 3)
+
+    x = x0 + S[1:end-1]
+    p = p0 + S[end]
+
+    dx = x
+
+    f!(dx,x,p,t)
+
+    F1 = dx(ceros)
+    F2 = [sum([(derivative(dx[i],l)*derivative(dx[l],k))(ceros)*ν0[k] for l in 1:n, k in 1:n]) + κ0*ν0[i] for i in 1:n]
+    F3 = transpose(ν0)*ν0 - 1.0
+    F4 = transpose(w0)*ν0
+    return [F1; F2; F3; F4]
+end
+
+#-
+
+function Hopf_Jacobian(f!,x0,p0,ν0,κ0,t,w0)
+
+    n = length(x0)
+    ceros = zeros(n+1)
+
+    S = set_variables("s", numvars = n+1, order = 3)
+
+    x = x0 + S[1:end-1]
+    p = p0 + S[end]
+
+    dx = x
+
+    f!(dx,x,p,t)
+
+    A = [derivative(dx[i],j)(ceros) for i in 1:n, j in 1:n]
+    B = [derivative(dx[i],n+1)(ceros) for i in 1:n]
+    C = [0.0 for i in 1:n, j in 1:n]
+    D = zeros(n)
+    
+    E = [sum([derivative(derivative(dx[i],l)*derivative(dx[l],k)*ν0[k],j)(ceros) for  l in 1:n, k in 1:n]) for i in 1:n, j in 1:n]
+    F = [sum([derivative(derivative(dx[i],l)*derivative(dx[l],k)*ν0[k],n+1)(ceros) for  l in 1:n, k in 1:n]) for i in 1:n]
+    G = [sum([(derivative(dx[i],k)*derivative(dx[k],j))(ceros) for k in 1:n]) + ==(i,j)*κ0 for i in 1:n, j in 1:n]
+    K = ν0
+
+    L = zeros(1,n)
+    M = 0.0
+    N = 2*transpose(ν0)
+    Ñ = 0.0
+
+    O = zeros(1,n)
+    P = 0.0
+    Q = transpose(w0)
+    R = 0.0
+
+    Extended_Jacobian = [A B C D;
+                         E F G K;
+                         L M N Ñ;
+                         O P Q R]
+    
+    return Extended_Jacobian
+end
+
+#-
+
 
 # ### Referencias
 
