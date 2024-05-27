b7cd77df2282b0453012834cd9a33f33cc810cf2
Leonel Mayorga López <leoml141.lml@gmail.com>
2024-05-27 14:56:04 -0600

    Anadi metodos para graficar bifurcaciones

--- examples/Predator-Prey model/Predator-Prey.jl
+++ examples/Predator-Prey model/Predator-Prey.jl
@@ -1,7 +1,8 @@
 include("../../src/NumDiffEq.jl");
 
-using .NumDiffEq
-using Plots, TaylorSeries, LinearAlgebra
+#-
+
+using Plots;
 
 #-
 
@@ -24,7 +25,8 @@ end;
 
 tiempo = @elapsed begin
     ramas_de_equilibrio = Equilibrium.(f!, x_ini, p_ini, t, Δs, p_fin)
-    LBP = Limit_Points.(f!,ramas_de_equilibrio,Δs,t)
+    LBP = Limit_Points.(f!,ramas_de_equilibrio,t,p_fin)
+    HBP = Hopf_Points.(f!,ramas_de_equilibrio,t,p_fin)
     estabilidad = Stability_intervals.(f!,t,ramas_de_equilibrio)
 end;
 
@@ -32,6 +34,7 @@ end;
 
 begin
     LBP_2D = [[(LBP[i][j][1],LBP[i][j][2][1]) for j in 1:length(LBP[i])] for i in 1:length(LBP)]
+    HBP_2D = [[(HBP[i][j][1],HBP[i][j][2][1]) for j in 1:length(HBP[i])] for i in 1:length(HBP)]
     estable = [[(estabilidad[i][1][j][1],estabilidad[i][1][j][2][1]) for j in 1:length(estabilidad[i][1])] for i in 1:length(estabilidad)]
     inestable = [[(estabilidad[i][2][j][1],estabilidad[i][2][j][2][1]) for j in 1:length(estabilidad[i][2])] for i in 1:length(estabilidad)]
 end;
@@ -43,6 +46,7 @@ begin
     plot!(estable[1], label = "Estable", linestyle = :solid, color = "blue")
     plot!(inestable[1], label = "Inestable", linestyle = :dash, color = "blue")
     scatter!(LBP_2D[1], label = "Bifurcación LP", color = "white")
+    scatter!(HBP_2D[1], label = "Bifurcación Hopf", color = "red")
     ylims!(-0.1,1.0)
     xlims!(0.0,0.9)
 
@@ -50,6 +54,7 @@ begin
     plot!(estable[i], label = "", linestyle = :solid, color = "blue")
     plot!(inestable[i], label = "", linestyle = :dash, color = "blue")
     scatter!(LBP_2D[i], label = "", color = "white")
+    scatter!(HBP_2D[i], label = "", color = "red")
     end
 
     savefig("Predator-Prey fish.png")
@@ -59,6 +64,7 @@ end;
 
 begin
     LBP_3D = [[(LBP[i][j][2][2],LBP[i][j][1],LBP[i][j][2][1]) for j in 1:length(LBP[i])] for i in 1:length(LBP)]
+    HBP_3D = [[(HBP[i][j][2][2],HBP[i][j][1],HBP[i][j][2][1]) for j in 1:length(HBP[i])] for i in 1:length(HBP)]
 
     plot(title = "Predator-Prey Model \n Tiempo de ejecución = $(tiempo) s", ylabel = "Quota", xlabel = "Sharks", zlabel = "Fish", camera = (60, 25))
 
@@ -87,10 +93,12 @@ begin
             plot!(y_estable,x,z_estable,xflip = true, color = "blue",linestyle = :solid, label = "Estable")
             plot!(y_inestable,x,z_inestable,xflip = true, color = "blue",linestyle = :dash, label = "Inestable")
             scatter!(LBP_3D[i], label = "Bifurcación LP", color = "white")
+            scatter!(HBP_3D[i], label = "Bifurcación Hopf", color = "red")
         else
             plot!(y_estable,x,z_estable,xflip = true, color = "blue",linestyle = :solid, label = "")
             plot!(y_inestable,x,z_inestable,xflip = true, color = "blue",linestyle = :dash, label = "")
             scatter!(LBP_3D[i], label = "", color = "white")
+            scatter!(HBP_3D[i], label = "", color = "red")
         end
     end
 
@@ -104,100 +112,24 @@ end
 #-
 
 begin
+    #=
     for i in 1:length(x_ini)
         normas = []
-        for j in 1:length(solfams[i][1])
+        for j in 1:length(ramas_de_equilibrio[i][1])
             dx = zeros(length(x_ini))
-            f!(dx,solfams[i][2][j],solfams[i][1][j],t)
+            f!(dx,ramas_de_equilibrio[i][2][j],ramas_de_equilibrio[i][1][j],t)
             push!(normas,norm(dx))
         end
         if i == 2
             precision = plot(title = "Precisión de la rama", ylabel = "||F(u(λ),λ)||", xlabel = "λ")
-            plot!(solfams[i][1],normas ,label = "λ_ini = $(p_ini), x_ini = $(x_ini[i])")
+            plot!(ramas_de_equilibrio[i][1],normas ,label = "λ_ini = $(p_ini), x_ini = $(x_ini[i])")
         else
             precision = plot(title = "Precisión de la rama", ylabel = "||F(u(λ),λ)|| / 1×10⁻¹⁶", xlabel = "λ")
-            plot!(solfams[i][1],normas .* 1.e16 ,label = "λ_ini = $(p_ini), x_ini = $(x_ini[i])")
+            plot!(ramas_de_equilibrio[i][1],normas .* 1.e16 ,label = "λ_ini = $(p_ini), x_ini = $(x_ini[i])")
         end
         savefig("PrecisiónRama$(i).png")
     end
+    =#
 end
 
 #-
-
-function ν_initial(f!,t,x0,p0)
-
-    n = length(x0)
-    ceros = zeros(n+1)
-
-    S = set_variables("s", numvars = n+1, order = 3)
-
-    x = x0 + S[1:end-1]
-    p = p0 + S[end]
-
-    dx = x
-
-    f!(dx,x,p,t)
-
-    Fx = [derivative(dx[i],j)(ceros) for i in 1:n, j in 1:n]
-
-    ν = [1.0,1.0]
-    ω = imag(eigvals(Fx)[1])
-
-    for i in 1:100
-
-        μ = (Fx^2 + ω^2*I(n))\ν
-
-        ν = μ/norm(μ)
-
-    end
-
-    return ν
-
-end
-
-prueba = Equilibrium(f!, x_ini[3], p_ini, t, Δs, p_fin; N = 10000)
-
-p0, x0, ν0, λ0, w0 = Hopf_initial_values(f!,prueba,t)[1]
-
-x = x0
-p = p0
-κ = imag(λ0[1])^2
-ν = real(ν0[:,1])
-w = w0[:,1]
-
-ν = ν_initial(f!,t,x,p)
-w = [ν[2],-ν[1]]
-
-ν ⋅ w
-
-Hopf_function(f!,x,p,ν,ω^2,t,w)
-
-i = 1
-
-while i <= 30 && norm(Hopf_function(f!,x,p,ν,κ,t,w)) > 1.e-16
-    println("Paso $(i): λ = $(p)")
-    println("         x = $(x)")
-    println("         ν = $(ν)")
-    println("         κ = $(κ)")
-    println("         w = $(w)")
-    println("         norm(Hopf_function) = $(norm(Hopf_function(f!,x,p,ν,κ,t,w)))")
-    println("\n")
-    show(stdout, "text/plain",  Hopf_function(f!,x,p,ν,κ,t,w))
-    println("\n")
-
-    X = [x;p;ν;κ] - inv(Hopf_Jacobian(f!,x,p,ν,κ,t,w))*Hopf_function(f!,x,p,ν,κ,t,w)
-    x = X[1:length(x)]
-    p = X[length(x)+1]
-    ν = X[length(x)+2:2*length(x)+1]
-    κ = X[end]
-
-    i += 1
-end
-
-norm(Hopf_function(f!,x,p,ν,κ,t,w))
-
-x
-p
-ν
-κ
-
--- examples/example1D/example1D.jl
+++ examples/example1D/example1D.jl
@@ -1,6 +1,4 @@
-include("../../src/equilibrium.jl");
-include("../../src/bifurcation.jl");
-include("../../src/stability.jl");
+include("../../src/NumDiffEq.jl");
 
 #-
 
@@ -24,10 +22,14 @@ end;
 #-
 
 tiempo = @elapsed begin
-solfams = Equilibrium.(g, x_ini, p_ini, t, Δs, p_fin);
-bifurcaciones = Bifurcation_point.(g,x_ini,p_ini,t)
-estabilidad = Stability_intervals.(g,t,solfams)
+ramas_de_equilibrio = Equilibrium.(g, x_ini, p_ini, t, Δs, p_fin);
+LBP = Limit_Points.(g,ramas_de_equilibrio,t,p_fin)
+estabilidad = Stability_intervals.(g,t,ramas_de_equilibrio)
 end;
+
+#-
+
+LBP = [[(LBP[i][j][1],LBP[i][j][2]) for j in 1:length(LBP[i])] for i in 1:length(LBP)];
 
 #-
 
@@ -35,12 +37,12 @@ begin
     plot(title = "Puntos de equilibrio \n Tiempo de ejecución = $(tiempo) s", ylabel = "u(λ)", xlabel = "λ",legendfont=font(5))
     plot!(estabilidad[1][1], label = "Puntos estables", color = "blue", linestyle = :solid)
     plot!(estabilidad[1][2], label = "Puntos inestables", color = "blue", linestyle = :dash)
+    scatter!(LBP[1], label = "Bifurcaciones LP", color = "white")
     for i in 2:length(estabilidad)
         plot!(estabilidad[i][1], label = "", color = "blue", linestyle = :solid)
         plot!(estabilidad[i][2], label = "", color = "blue", linestyle = :dash)
+        scatter!(LBP[i], label = "", color = "white")
     end
-    scatter!(bifurcaciones[1],color = "red", label = "Bifurcación", markersize = 3)
-    scatter!(bifurcaciones[2:end],color = "red", label = "", markersize = 3)
     savefig("Ejemplo 1D.png")
 end;
 
@@ -49,40 +51,15 @@ end;
 begin
     precisiones = []
 
-    for i in 1:length(x_ini)
+    #=for i in 1:length(x_ini)
         precisioni = plot(title = "λ_ini = $(p_ini), x_ini = $(x_ini[i])", ylabel = "|g(u(λ),λ)| / 1×10⁻¹⁶", titlefontsize = 7, xlabel = "λ", guidefontsize = 8)
-        plot!(solfams[i][1],abs.(g.(solfams[i][2],solfams[i][1],t)) .* 1.e16 ,label = "")
+        plot!(ramas_de_equilibrio[i][1],abs.(g.(ramas_de_equilibrio[i][2],ramas_de_equilibrio[i][1],t)) ,label = "")
         push!(precisiones,precisioni)
-    end
+    end=#
 
-    plot(precisiones..., layout = (1,2),plot_title="Precisión de las ramas de soluciones",plot_titlefontsize = 10)
+    #plot(precisiones..., layout = (1,2),plot_title="Precisión de las ramas de soluciones",plot_titlefontsize = 10)
 
-    savefig("Precision.png")
+    #savefig("Precision.png")
 end;
 
 #-
-
-A = [rand() for i in 1:3,j in 1:3]
-
-det(A)
-
-L, U, P = factorize(A)
-
-
-L
-U
-P
-
-A
-
-P
-
-[i == j ? 1 : 0 for i in 1:length(P), j in P]*L*U
-
--det(U)
-
-det([i == j ? 1 : 0 for i in 1:length(P), j in P])
-
-det(L)
-
-det(A)
