ae12d63ab4cf836543b6d53b479fb13d7613c2c9
Leonel Mayorga López <leoml141.lml@gmail.com>
2024-05-27 14:54:11 -0600

    Anadi un archivo con todas las funciones

---
+++ src/NumDiffEq.jl
@@ -0,0 +1,9 @@
+
+using TaylorSeries, LinearAlgebra
+
+include("equilibrium_functions.jl")
+include("equilibrium.jl")
+include("stability.jl")
+include("limit_point_functions.jl")
+include("hopf_point_functions.jl")
+include("bifurcation.jl")
